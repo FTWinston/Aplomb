07eb7c87d8b01170c54362179946e948de1f65da
Andrew <andrew.watkins@cctechnology.com>
2015-12-23 10:10:25 +0000

    Added some fancy routing, so that requests will go to static content controllers where present, and otherwise, will look for dynamic content

--- Aplomb/App_Start/RouteConfig.cs
+++ Aplomb/App_Start/RouteConfig.cs
@@ -14,10 +14,41 @@ namespace Aplomb
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
+                name: "Static",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                constraints: new { controller = new StaticPageConstraint() }
             );
+
+            routes.MapRoute(
+                name: "Dynamic",
+                url: "{section}/{page}/{id}",
+                defaults: new { controller = "Page", action = "Display", page = UrlParameter.Optional, id = UrlParameter.Optional }
+            );
+        }
+
+        public class StaticPageConstraint : IRouteConstraint
+        {
+            private static SortedSet<string> siteControllers = null;
+
+            public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+            {
+                if (siteControllers == null)
+                    PopulateControllerList();
+
+                string value = values[parameterName].ToString();
+                return siteControllers.Contains(value);
+            }
+
+            private void PopulateControllerList()
+            {
+                const string suffix = "Controller";
+                siteControllers = new SortedSet<string>(StringComparer.InvariantCultureIgnoreCase);
+                var types = GetType().Assembly.GetTypes().Where(t => t.Namespace == "Aplomb.Controllers" && t.Name.EndsWith(suffix) & t != typeof(Aplomb.Controllers.PageController));
+
+                foreach (var type in types)
+                    siteControllers.Add(type.Name.Substring(0, type.Name.Length - suffix.Length));
+            }
         }
     }
 }
---
+++ Aplomb/Controllers/PageController.cs
@@ -0,0 +1,26 @@
+﻿using Aplomb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aplomb.Controllers
+{
+    public class PageController : Controller
+    {
+        public ActionResult Display(string section, string page, int? id)
+        {
+            DataModel dm = new DataModel();
+            var pageModel = dm.Pages.Where(p => p.Section.Slug == section && p.Slug == page).SingleOrDefault();
+
+            if (pageModel == null)
+                return HttpNotFound();
+
+            ViewBag.Section = pageModel.Section.Name;
+            ViewBag.Page = pageModel.Slug;
+            ViewBag.ID = id;
+            return View("Test");
+        }
+	}
+}
